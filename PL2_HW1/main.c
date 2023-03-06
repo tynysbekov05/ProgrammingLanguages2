87c36f2da7d7342430c119be16020dd69c65ce65
kaisartynysbekov <tynysbekov05@bk.ru>
2023-03-06 15:02:56 +0300

    Initial Commit

---
+++ PL2_HW1/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  PL2_HW1
+//
+//  Created by Кайсар Тынысбеков on 06.03.2023.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
